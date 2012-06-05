c3a3cc71d4dacdf4683832c592cf37d65cef1355
Qwlouse <klaus.greff@gmx.net>
2012-06-05 16:15:53 +0200

    moved some methods out of invoke()

--- Cameraman.py
+++ Cameraman.py
@@ -4,20 +4,137 @@
 from os import path
 from mathutils import Vector, Euler
 import bpy
-#from multiprocessing import Process, Queue, Lock, Manager
-import subprocess
-#import random
-#import math
-import time
 
-from Config import PROJECT_PATH
+import subprocess
+
+
+from Config import PROJECT_PATH, SHOT_NAMES
 
 position_process_filename = path.abspath(path.join(project_path, "PositionProcess.py"))
 beatscript_classifier_filename = path.abspath(path.join(project_path, "BeatscriptClassifier.sh"))
 
+
+
+
+def setConfiguration(newConfiguration, camera):
+    camera.location = newConfiguration[0]
+    camera.rotation_euler = newConfiguration[1]
+    camera.keyframe_insert(data_path="rotation_euler")
+    camera.keyframe_insert(data_path="location")
+
+
+
+
+
 class AutomoculusCameraman(bpy.types.Operator):
     bl_idname = "marker.automoculus"
     bl_label = "Automoculus - position camera"
+
+
+    def calculateForNewBeats(self, classificationProcess, shot, frame, lastcut, initialcut, camera, scenicContext):
+        # New Beats! That changes the situation: what's the distribution now?
+        classificationProcess.stdin.write(b'p\n')
+        classificationProcess.stdin.flush()
+        dist = self.getDistFromStr(classificationProcess.stdout.readline().decode('utf-8').rstrip())
+        #print(str(classificationProcess.stdout.readline(), encoding='utf-8').rstrip())
+        #dist = [0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14]
+
+        # For new beats we have to update the targets
+        classificationProcess.stdin.write(b't\n')
+        classificationProcess.stdin.flush()
+        target, linetarget = self.getTargetsFromStr(
+            classificationProcess.stdout.readline().decode('utf-8').rstrip())
+        #target = bpy.data.objects["Green"]
+        #linetarget = bpy.data.objects["Red"]
+        print(target.name + "\t" + linetarget.name)
+
+        # Should we cut? Ask the classificationProcess
+        classificationProcess.stdin.write(b'c\n')
+        classificationProcess.stdin.flush()
+        cut = classificationProcess.stdout.readline().decode('utf-8').rstrip() == "yes"
+        print("Cut: " + str(cut))
+
+        # Calculate optimal positions for all shots in the distribution with a propability > 0
+        #processes = []
+        #returnqueues = []
+        #for i in range(0, len(SHOT_NAMES)):
+        #    if dist[i] > 0:
+        #        returnqueues.append(Queue())
+        #        returnqueues[i].put(i)
+        #        processes.append(Process(target=calculatePosition, args=(i != shot or cut, i, returnqueues[i])))
+        #        processes[i].start()
+
+        # Determine which shot fits best, regarding the classified propability
+        bestratio = 0
+        bestconfig = (camera.location, camera.rotation_euler)
+        bestShotCandidate = shot
+        noCutRatio = 0
+        noCutConfig = (camera.location, camera.rotation_euler)
+        correction = [141, 42, 59, 130, 130, 130, 130]
+        #correction = [100, 100, 100, 100, 100, 100, 100]
+        shots = []
+        for i in range(0, len(SHOT_NAMES)):
+            if dist[i] > 0.1 or i == shot:
+                shots.append(i)
+        candidatestr = "Es kommen folgende Einstellungsgrößen in Frage: "
+        for shot in shots:
+            candidatestr += SHOT_NAMES[shot]+", "
+        print(candidatestr)
+        results = self.cameraOptimizer(scenicContext, target, linetarget, shots,
+            (camera.location, camera.rotation_euler))
+        for result in results:
+            configuration = result[0]
+            fitness = result[1]
+            shot_number = result[2]
+            print("Fitness " + str(fitness) + " for " + SHOT_NAMES[shot_number] + ".")
+            ratio = 1 / fitness * dist[shot_number] * correction[shot_number]
+            print("Ratio " + str(ratio) + " for " + SHOT_NAMES[shot_number] + ".")
+            if shot_number == shot:
+                noCutRatio = ratio
+                noCutConfig = configuration
+            if ratio > bestratio:
+                bestratio = ratio
+                bestconfig = configuration
+                bestShotCandidate = shot_number
+
+        # Should we Cut?
+        if bestratio - 0.1 > noCutRatio or initialcut or cut: # We want to cut, the ratio gets much better
+            shot = bestShotCandidate
+            if (bestconfig[0] - camera.location).length < 0.8:
+                #newConfiguration = self.springConfigurator(bestconfig,
+                #    (camera.location, camera.rotation_euler))
+                newConfiguration = bestconfig
+                print("Es sollte geschnitten werden, die Abweichung war jedoch zu gering. Wir bleiben bei " +
+                      SHOT_NAMES[shot])
+            else:
+                newConfiguration = bestconfig
+                lastcut = frame
+                self.setInitialVelocity(target)
+                print("Schnitt auf: " + SHOT_NAMES[shot])
+        else: # We don't want to cut because the ratio doesn't get significantly better
+            #newConfiguration = self.springConfigurator(noCutConfig,
+            #    (camera.location, camera.rotation_euler))
+            newConfiguration = noCutConfig
+            print("Kein Schnitt. Wir bleiben bei " + SHOT_NAMES[shot])
+            # Tell our decision to the classifier
+        classificationProcess.stdin.write(b'd\n')
+        classificationProcess.stdin.flush()
+        classificationProcess.stdin.write((str(shot) + "\n").encode('utf-8'))
+        classificationProcess.stdin.flush()
+        if classificationProcess.stdout.readline().decode('utf-8').rstrip() != "decision recieved":
+            print("Could not Write decision.")
+        return newConfiguration, shot, lastcut, target, linetarget
+
+
+    def setInitialVelocity(self, target):
+        # Geschwindigkeit des Targets auf die Kamera übertragen.
+        target_tmp_loc = Vector((target.location[0], target.location[1], target.location[2]))
+        bpy.data.scenes['Scene'].frame_current -= 1
+        bpy.ops.object.paths_calculate()
+        target_velocity = (target_tmp_loc - target.location) * 1.333333
+        bpy.data.scenes['Scene'].frame_current += 1
+        bpy.ops.object.paths_calculate()
+        self.velocity = (target_velocity, Euler((0, 0, 0), 'XYZ'))
 
     def createScenicContext(self, process):
         context = {"persons": [], "objects": []}
@@ -45,6 +162,8 @@ class AutomoculusCameraman(bpy.types.Operator):
             vectorCoordStrings = parts[1].split("|")
             return location, Euler(
                 (float(vectorCoordStrings[0]), float(vectorCoordStrings[1]), float(vectorCoordStrings[2])), 'XYZ')
+
+
         position_process_filename = path.join(PROJECT_PATH, "PositionProcess.py")
         optimizationProcess = subprocess.Popen(
             ['python', position_process_filename]
@@ -122,119 +241,6 @@ class AutomoculusCameraman(bpy.types.Operator):
 
 
     def invoke(self, context, event):
-        def setInitialVelocity(target):
-            # Geschwindigkeit des Targets auf die Kamera übertragen.
-            target_tmp_loc = Vector((target.location[0], target.location[1], target.location[2]))
-            bpy.data.scenes['Scene'].frame_current -= 1
-            bpy.ops.object.paths_calculate()
-            target_velocity = (target_tmp_loc - target.location) * 1.333333
-            bpy.data.scenes['Scene'].frame_current += 1
-            bpy.ops.object.paths_calculate()
-            self.velocity = (target_velocity, Euler((0, 0, 0), 'XYZ'))
-
-        def setConfiguration(newConfiguration):
-            camera.location = newConfiguration[0]
-            camera.rotation_euler = newConfiguration[1]
-            camera.keyframe_insert(data_path="rotation_euler")
-            camera.keyframe_insert(data_path="location")
-
-        def calculateForNewBeats(classificationProcess, shot, frame, lastcut, initialcut):
-            # New Beats! That changes the situation: what's the distribution now?
-            classificationProcess.stdin.write(b'p\n')
-            classificationProcess.stdin.flush()
-            dist = self.getDistFromStr(classificationProcess.stdout.readline().decode('utf-8').rstrip())
-            #print(str(classificationProcess.stdout.readline(), encoding='utf-8').rstrip())
-            #dist = [0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14]
-
-            # For new beats we have to update the targets
-            classificationProcess.stdin.write(b't\n')
-            classificationProcess.stdin.flush()
-            target, linetarget = self.getTargetsFromStr(
-                classificationProcess.stdout.readline().decode('utf-8').rstrip())
-            #target = bpy.data.objects["Green"]
-            #linetarget = bpy.data.objects["Red"]
-            print(target.name + "\t" + linetarget.name)
-
-            # Should we cut? Ask the classificationProcess
-            classificationProcess.stdin.write(b'c\n')
-            classificationProcess.stdin.flush()
-            cut = classificationProcess.stdout.readline().decode('utf-8').rstrip() == "yes"
-            print("Cut: " + str(cut))
-
-            # Calculate optimal positions for all shots in the distribution with a propability > 0
-            #processes = []
-            #returnqueues = []
-            #for i in range(0, len(SHOT_NAMES)):
-            #    if dist[i] > 0:
-            #        returnqueues.append(Queue())
-            #        returnqueues[i].put(i)
-            #        processes.append(Process(target=calculatePosition, args=(i != shot or cut, i, returnqueues[i])))
-            #        processes[i].start()
-
-            # Determine which shot fits best, regarding the classified propability
-            bestratio = 0
-            bestconfig = (camera.location, camera.rotation_euler)
-            bestShotCandidate = shot
-            noCutRatio = 0
-            noCutConfig = (camera.location, camera.rotation_euler)
-            correction = [141, 42, 59, 130, 130, 130, 130]
-            #correction = [100, 100, 100, 100, 100, 100, 100]
-            shots = []
-            for i in range(0, len(SHOT_NAMES)):
-                if dist[i] > 0.1 or i == shot:
-                    shots.append(i)
-            candidatestr = "Es kommen folgende Einstellungsgrößen in Frage: "
-            for shot in shots:
-                candidatestr += SHOT_NAMES[shot]+", "
-            print(candidatestr)
-            results = self.cameraOptimizer(scenicContext, target, linetarget, shots,
-                (camera.location, camera.rotation_euler))
-            for result in results:
-                configuration = result[0]
-                fitness = result[1]
-                shot_number = result[2]
-                print("Fitness " + str(fitness) + " for " + SHOT_NAMES[shot_number] + ".")
-                ratio = 1 / fitness * dist[shot_number] * correction[shot_number]
-                print("Ratio " + str(ratio) + " for " + SHOT_NAMES[shot_number] + ".")
-                if shot_number == shot:
-                    noCutRatio = ratio
-                    noCutConfig = configuration
-                if ratio > bestratio:
-                    bestratio = ratio
-                    bestconfig = configuration
-                    bestShotCandidate = shot_number
-
-            # Should we Cut?
-            if bestratio - 0.1 > noCutRatio or initialcut or cut: # We want to cut, the ratio gets much better
-                shot = bestShotCandidate
-                if (bestconfig[0] - camera.location).length < 0.8:
-                    #newConfiguration = self.springConfigurator(bestconfig,
-                    #    (camera.location, camera.rotation_euler))
-                    newConfiguration = bestconfig
-                    print("Es sollte geschnitten werden, die Abweichung war jedoch zu gering. Wir bleiben bei " +
-                          SHOT_NAMES[shot])
-                else:
-                    newConfiguration = bestconfig
-                    lastcut = frame
-                    setInitialVelocity(target)
-                    print("Schnitt auf: " + SHOT_NAMES[shot])
-            else: # We don't want to cut because the ratio doesn't get significantly better
-                #newConfiguration = self.springConfigurator(noCutConfig,
-                #    (camera.location, camera.rotation_euler))
-                newConfiguration = noCutConfig
-                print("Kein Schnitt. Wir bleiben bei " + SHOT_NAMES[shot])
-                # Tell our decision to the classifier
-            classificationProcess.stdin.write(b'd\n')
-            classificationProcess.stdin.flush()
-            classificationProcess.stdin.write((str(shot) + "\n").encode('utf-8'))
-            classificationProcess.stdin.flush()
-            if classificationProcess.stdout.readline().decode('utf-8').rstrip() != "decision recieved":
-                print("Could not Write decision.")
-            return newConfiguration, shot, lastcut, target, linetarget
-
-        SHOT_NAMES = ["detail", "closeup", "medium_shot", "american_shot", "full_shot", "long_shot",
-                      "extreme_long_shot"]
-        #blockcount = 0
         classification_process_filename = path.join(PROJECT_PATH, "BeatscriptClassifier.py")
         classificationProcess = subprocess.Popen(
             [classification_process_filename,
@@ -251,12 +257,12 @@ class AutomoculusCameraman(bpy.types.Operator):
         classificationProcess.stdin.write(b't\n')
         classificationProcess.stdin.flush()
         target, linetarget = self.getTargetsFromStr(classificationProcess.stdout.readline().decode('utf-8').rstrip())
-        setInitialVelocity(target)
+        self.setInitialVelocity(target)
         bpy.data.scenes['Scene'].frame_current = 1
         bpy.ops.object.paths_calculate()
-        newConfiguration, shot, lastcut, target, linetarget = calculateForNewBeats(classificationProcess, shot, 1,
-            lastcut, True)
-        setConfiguration(newConfiguration)
+        newConfiguration, shot, lastcut, target, linetarget = self.calculateForNewBeats(classificationProcess, shot, 1,
+            lastcut, True, camera, scenicContext)
+        setConfiguration(newConfiguration, camera)
         print("Szenenkontext erzeugt.")
         for frame in range(2, bpy.context.scene.frame_end):
             bpy.data.scenes['Scene'].frame_current = frame
@@ -270,9 +276,9 @@ class AutomoculusCameraman(bpy.types.Operator):
                 classificationProcess.stdin.flush()
                 if classificationProcess.stdout.readline().decode('utf-8').rstrip() == "yes": # there are new beats
                     print("Neue Beats, neues Glück!")
-                    newConfiguration, shot, lastcut, target, linetarget = calculateForNewBeats(classificationProcess,
+                    newConfiguration, shot, lastcut, target, linetarget = self.calculateForNewBeats(classificationProcess,
                         shot, frame, lastcut,
-                        False)
+                        False, camera, scenicContext)
                 else: # There were no new beats
                     print("Keine neuen Beats.")
                     optimalConfiguration, fitness, notused = self.cameraOptimizer(scenicContext, target, linetarget,
@@ -290,7 +296,7 @@ class AutomoculusCameraman(bpy.types.Operator):
                 newConfiguration = self.springConfigurator(optimalConfiguration,
                     (camera.location, camera.rotation_euler))
                 #newConfiguration = optimalConfiguration
-            setConfiguration(newConfiguration)
+            setConfiguration(newConfiguration, camera)
             bpy.data.scenes['Scene'].camera.data.dof_distance = (target.location - camera.location).length
             camera.data.keyframe_insert(data_path="dof_distance")
         classificationProcess.stdin.write(b'q\n')
