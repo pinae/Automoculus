e4e5d54ce38cd7b6a05e49b30a64cd000cc09174
pinae <pinae@web.de>
2012-08-03 12:20:52 +0200

    New Beatscripts

--- Config.py
+++ Config.py
@@ -12,13 +12,18 @@ DETAIL, CLOSEUP, MEDIUM_SHOT, AMERICAN_SHOT, FULL_SHOT, LONG_SHOT, EXTREME_LONG_
 SHOT_NAMES = ["detail", "closeup", "medium_shot", "american_shot", "full_shot", "long_shot", "extreme_long_shot"]
 PERSON, OBJECT, PLACE = range(0, 3)
 DEMONSTRAT_TYPE_NAMES = ["person", "object", "place"]
-TRAIN_FILES = ["Berg der Versuchung - Schwere Stelle.csv", "Double Indemnity - Eingang.csv",
-               "Double Indemnity - Identifikation.csv", "Double Indemnity - Leiche.csv",
+TRAIN_FILES = ["Berg der Versuchung - Schwere Stelle.csv",
+               "Double Indemnity - Eingang.csv",
+               "Double Indemnity - Identifikation.csv",
+               "Double Indemnity - Leiche.csv",
                "Double Indemnity - Telefonat.csv",
-               "Ivanhoe - Ende.csv", "Ivanhoe - Endkampf.csv",
-               "Ivanhoe - Gefangennahme.csv", "Ivanhoe - Taverne.csv",
+               "Ivanhoe - Ende.csv",
+               "Ivanhoe - Endkampf.csv",
+               "Ivanhoe - Gefangennahme.csv",
+               "Ivanhoe - Taverne.csv",
                "Ivanhoe - Turnier.csv",
-               "The Count of Monte Christo - Messerkampf.csv", "The Count of Monte Christo - Verschwörer.csv",
+               "The Count of Monte Christo - Messerkampf.csv",
+               "The Count of Monte Christo - Verschwörer.csv",
                "The Maltese Falcon - Beginn.csv",
                "The Treasure of the Sierra Madre - Ausbeuter.csv",
                "The Treasure of the Sierra Madre - Banditen.csv",
@@ -27,7 +32,9 @@ TRAIN_FILES = ["Berg der Versuchung - Schwere Stelle.csv", "Double Indemnity - E
                "The Treasure of the Sierra Madre - Goldverlust.csv",
                "The Woman in the Window - Abenteuerlust.csv",
                "The Woman in the Window - Ampel.csv",
-               "The Woman in the Window - Bar.csv"]
+               "The Woman in the Window - Bar.csv",
+               "The Woman in the Window - Familienszene.csv",
+               "The Woman in the Window - Mord.csv"]
 
 PROJECT_PATH = path.dirname(path.abspath(__file__))
 
