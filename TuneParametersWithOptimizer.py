61d8874806fef2201e01664b3b0aaf068885b31e
pinae <pinae@web.de>
2012-08-16 14:11:37 +0200

    starting optimization with default values

--- TuneParametersWithOptimizer.py
+++ TuneParametersWithOptimizer.py
@@ -9,11 +9,11 @@ from Classify import getDataMatrix
 from Config import TRAIN_FILES
 from XValidation import ParallelXValidation
 
-def tuneParametersForSVM(files, scaler, fake_decisions=False):
+def tuneParametersForSVM(files, scaler, reference_data, fake_decisions=False):
     def fitnessFunction(parameters, files, scaler, fake_decisions):
         return ParallelXValidation(files, scaler, fake_decisions, C=max(0.0,parameters[0]), gamma=max(1e-323,parameters[1]))
 
-    start_vector = np.array([1000, 0.00001])
+    start_vector = np.array([len(reference_data), 1.0/len(reference_data[0])])
     print("Starting optimization.")
     tuned_parameters = opt.fmin_powell(func=fitnessFunction, x0=start_vector, args=(files, scaler, fake_decisions))
     print("____________________________________")
@@ -26,7 +26,7 @@ def main():
     reference_data, _ = getDataMatrix(TRAIN_FILES)
     scaler = preprocessing.Scaler()
     scaler.fit(reference_data)
-    tuneParametersForSVM(TRAIN_FILES, scaler, True)
+    tuneParametersForSVM(TRAIN_FILES, scaler, reference_data, True)
 
 if __name__ == "__main__":
     main()
