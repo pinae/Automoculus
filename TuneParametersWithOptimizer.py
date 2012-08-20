2ad3b2a1b865643f2d15521cc0bcfd3e7c5382b5
pinae <pinae@web.de>
2012-08-20 22:01:33 +0200

    pre-optimized start-values for optimization

--- TuneParametersWithOptimizer.py
+++ TuneParametersWithOptimizer.py
@@ -13,7 +13,8 @@ def tuneParametersForSVM(files, scaler, reference_data, fake_decisions=False):
     def fitnessFunction(parameters, files, scaler, fake_decisions):
         return ParallelXValidation(files, scaler, fake_decisions, C=max(0.0,parameters[0]), gamma=max(1e-323,parameters[1]))
 
-    start_vector = np.array([len(reference_data), 1.0/len(reference_data[0])])
+    #start_vector = np.array([len(reference_data), 1.0/len(reference_data[0])])
+    start_vector = np.array([2582.61517656, 0.00036375303213])
     print("Starting optimization.")
     tuned_parameters = opt.fmin_powell(func=fitnessFunction, x0=start_vector, args=(files, scaler, fake_decisions))
     print("____________________________________")
