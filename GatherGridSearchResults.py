b579551619e086813747f189ba300032d13cd026
pinae <pinae@web.de>
2012-09-04 00:30:20 +0200

    nice diagram

--- GatherGridSearchResults.py
+++ GatherGridSearchResults.py
@@ -4,6 +4,7 @@
 from __future__ import division
 import sys, os
 import numpy as np
+from matplotlib.colors import LogNorm
 from matplotlib.patches import Patch
 from pylab import *
 
@@ -23,14 +24,15 @@ def gather_results(path):
         else:
             results[C] = {gamma: result}
     result_table = []
-    gammas = []
-    for gamma in results[results.keys()[0]]:
-        gammas.append(gamma)
     Cs = []
     for C in results.keys():
         Cs.append(C)
     Cs.sort()
+    gammas = []
+    for gamma in results[max([results.keys()[x] for x in range(len(Cs))])]:
+        gammas.append(gamma)
     gammas.sort(reverse=True)
+    m_file = open(path+"../missing_notes.txt",'w')
     for C in Cs:
         result_line = []
         for gamma in gammas:
@@ -38,20 +40,27 @@ def gather_results(path):
                 result_line.append(results[C][gamma])
             else:
                 print("C="+str(C)+" gamma="+str(gamma)+" Result fehlt!")
-                result_line.append(2.7)
+                if (gamma < 0.144) and (gamma > 1.72e-09):
+                    m_file.write(str(C)+";"+str(gamma)+"\n")
+                result_line.append(2.647)
         result_table.append(np.array(result_line))
+    m_file.close()
     data = np.array(result_table)
     print Cs
     print gammas
     print data
-    X,Y = meshgrid(np.array(range(len(gammas)+1)), np.array(range(len(Cs)+1)))
-    #pcolor(X, Y, data)
-    #ax = subplot(111)
+    X,Y = meshgrid(np.array(gammas), np.array(Cs))
+    pcolor(X, Y, data)
+    ax = subplot(111)
     im = imshow(data, cmap=cm.jet)
     im.set_interpolation('nearest')
     #im.set_interpolation('bicubic')
     #im.set_interpolation('bilinear')
-    colorbar()
+    colorbar()#shrink=0.3)
+    autoscale(False)
+    axis([gammas[0],gammas[-1],Cs[0],Cs[-1]])
+    semilogx(gammas[-1],gammas[0],basex=10,label="gamma")
+    #ax.set_ylim(Cs[0],Cs[-1])
     show()
 
 if __name__ == "__main__":
