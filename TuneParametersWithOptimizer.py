9cd8f42bfebf1fc93325f1709873fc41d1bcc6bc
pinae <pinae@web.de>
2012-09-18 14:20:06 +0200

    max function calls parameter

--- TuneParametersWithOptimizer.py
+++ TuneParametersWithOptimizer.py
@@ -20,7 +20,7 @@ def tuneParametersForSVM(files, scaler, reference_data, fake_decisions=False, le
     print("gamma default: "+str(1.0/len(reference_data[0])))
     start_vector = np.array([1999.85770959, 6.30930490772e-07])
     print("Starting optimization.")
-    tuned_parameters = opt.fmin_powell(func=fitnessFunction, x0=start_vector, args=(files, scaler, fake_decisions, leave_out_class))
+    tuned_parameters = opt.fmin_powell(func=fitnessFunction, x0=start_vector, args=(files, scaler, fake_decisions, leave_out_class), maxfun=179)
     print("____________________________________")
     print("Results:")
     print("\tC="+str(tuned_parameters[0]))
